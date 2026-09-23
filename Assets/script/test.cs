610c3de5d845308853baafe5f451fef60f9049dc
rlfehd3235 <rlfehd3235@gmail.com>
2026-09-23 10:59:06 +0900

    깃허브 테스트

---
+++ Assets/script/test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("깃허브 테스트");
+    }
+
+  
+}
